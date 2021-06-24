f8751dfd97256931717a7def55eb2dc5fd3e7b75
khucvandoan <doankhuc123@gmail.com>
2021-06-24 10:36:48 +0700

    edit GUI login setstarted, change gender by radio

--- components/FormAccount/index.js
+++ components/FormAccount/index.js
@@ -1,16 +1,30 @@
 import React from "react";
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  Image,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import styles from "./styles";
 import default_background from "../../assets/default_background.png";
 
 const FormAccount = ({ children }) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.image_block}>
-        <Image source={default_background} style={styles.image} />
-      </View>
-      {children}
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{flex: 1}}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <View style={styles.image_block}>
+            <Image source={default_background} style={styles.image} />
+          </View>
+          {children}
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
